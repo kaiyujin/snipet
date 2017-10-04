b160814901401990dd0427ce36c6add536a0010b
daisuke shiraishi <kaiyujin@gmail.com>
2017-10-05 00:35:03 +0900

    金額を数字変換

--- pandas/clean.py
+++ pandas/clean.py
@@ -7,15 +7,30 @@ df = pd.read_csv('items_shops_detail_tokyo_1.csv')
 for i, v in df.iterrows():
     if v['lunch_price'] == 'null':
        v['lunch_price']  = ''
+
     if pd.isnull(v['sheats']):
         v['sheats']  = 0
     else:
         v['sheats']  = v['sheats'].replace('席','')
+
     if v['point'] == '-':
         v['point'] = 0
     else:
         v['point'] = float(v['point'])
 
+    if v['dinner_price'][-1:] == '～':
+        #max only
+        v['dinner_price'] = re.sub('[￥～, ]','',v['dinner_price'])
+        print(v['dinner_price'])
+    elif v['dinner_price'][1:2] == '～':
+        #min only
+        v['dinner_price'] = re.sub('[￥～, ]','',v['dinner_price'])
+        print(v['dinner_price'])
+    elif v['dinner_price'].find('～') > 0:
+        v['dinner_price'] = re.sub('[￥, ]','',v['dinner_price'])
+        min_max = v['dinner_price'].split('～')
+        print(str(min_max))
+
 #食べログ
-high = df.query('point >= 4 & resevation == "予約可"')
-high.to_csv('high.csv')
+#high = df.query('point >= 4 & resevation == "予約可"')
+#high.to_csv('high.csv')
